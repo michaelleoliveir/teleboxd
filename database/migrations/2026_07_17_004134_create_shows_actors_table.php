d600bbe12c94060c2ae54b95c86ca56dc75172e4
Michaelle Oliveira <michaelle.oliveira101103@gmail.com>
2026-07-16 21:48:38 -0300

    feat: create actors tables and association with shows

---
+++ database/migrations/2026_07_17_004134_create_shows_actors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('show_actor', function (Blueprint $table) {
+            $table->foreignId('show_id')->constrained('shows')->onDelete('cascade');
+            $table->foreignId('actor_id')->constrained('actors')->onDelete('cascade');
+            $table->string('character')->nullable();
+            $table->integer('popularity_order')->default(0)->nullable();
+            $table->primary(['show_id', 'actor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('show_actor');
+    }
+};
