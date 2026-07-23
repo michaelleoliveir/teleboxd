c6243ff3ba1fa529fa75aa5644fb407ae2843889
Michaelle Oliveira <michaelle.oliveira101103@gmail.com>
2026-07-22 21:34:08 -0300

    feat: create favorites, wishlist, and reviews tables with necessary relationships and fields

---
+++ database/migrations/2026_07_22_212327_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('show_id')->constrained('shows');
+            $table->text('content')->nullable();
+            $table->decimal('rating', 2, 1);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index(['show_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
